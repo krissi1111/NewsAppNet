4c63ccd24f6d1a4ff0476115a96903bb15891de0
Ket1 <ket1@hi.is>
2022-01-14 13:41:33 +0000

    Added new feed

---
+++ NewsAppNet/Data/NewsFeeds/RuvFeed.cs
@@ -0,0 +1,18 @@
+﻿using System.ServiceModel.Syndication;
+
+namespace NewsAppNet.Data.NewsFeeds
+{
+    public class RuvFeed : NewsFeedBase
+    {
+        public RuvFeed()
+        {
+            Url = "https://www.ruv.is/rss/frettir";
+            FeedName = "Ruv";
+        }
+
+        public override NewsItemBuilder BuildItem(SyndicationItem item)
+        {
+            return new RuvItemBuilder(item);
+        }
+    }
+}
---
+++ NewsAppNet/Data/NewsFeeds/RuvItemBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System.ServiceModel.Syndication;
+
+namespace NewsAppNet.Data.NewsFeeds
+{
+    public class RuvItemBuilder : NewsItemBuilder
+    {
+        public RuvItemBuilder(SyndicationItem item) : base(item) { }
+
+        public override string GetImage()
+        {
+            string link = GetLink();
+            using (HttpClient client = new())
+            {
+                string linkContent = client.GetStringAsync(link).Result;
+                int start = linkContent.IndexOf("https://www.ruv.is/sites/default/files/styles");
+                if (start == -1)
+                {
+                    return NoImage;
+                }
+                int stop = linkContent[start..].IndexOf(".jpg");
+                stop += 4;
+                return linkContent.Substring(start, stop);
+            }
+        }
+    }
+}
