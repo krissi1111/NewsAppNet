eac9cf263e77cb319965a9a51327acc6e5093a35
Ket1 <ket1@hi.is>
2022-01-21 17:42:31 +0000

    Title and summary are now optional

--- NewsAppNet/Models/ViewModels/Search.cs
+++ NewsAppNet/Models/ViewModels/Search.cs
@@ -2,8 +2,8 @@
 {
     public class Search
     {
-        public string Title { get; set; } = string.Empty;
-        public string Summary { get; set; } = string.Empty;
+        public string? Title { get; set; } = string.Empty;
+        public string? Summary { get; set; } = string.Empty;
         public string DateStart { get; set; } = new DateTime().ToString();
         public string DateEnd { get; set; } = DateTime.Now.ToString();
         public string Origin { get; set; } = string.Empty;
