dc919909fd7711042c08123449e8432dc8126858
Ket1 <ket1@hi.is>
2022-02-02 14:49:36 +0000

    Added http success status check

--- NewsAppNet/Data/NewsFeeds/Feeds/RuvFeed.cs
+++ NewsAppNet/Data/NewsFeeds/Feeds/RuvFeed.cs
@@ -1,19 +1,13 @@
 ﻿using NewsAppNet.Data.NewsFeeds.ItemBuilder;
-using System.ServiceModel.Syndication;
 
 namespace NewsAppNet.Data.NewsFeeds.Feeds
 {
-    public class RuvFeed : NewsFeedBase
+    public class RuvFeed : NewsFeedBase<RuvItemBuilder>
     {
         public RuvFeed()
         {
             Url = "https://www.ruv.is/rss/frettir";
             FeedName = "Ruv";
-        }
-
-        public override NewsItemBuilder BuildItem(SyndicationItem item)
-        {
-            return new RuvItemBuilder(item);
         }
     }
 }
--- NewsAppNet/Data/NewsFeeds/Feeds/VisirFeed.cs
+++ NewsAppNet/Data/NewsFeeds/Feeds/VisirFeed.cs
@@ -1,19 +1,13 @@
 ﻿using NewsAppNet.Data.NewsFeeds.ItemBuilder;
-using System.ServiceModel.Syndication;
 
 namespace NewsAppNet.Data.NewsFeeds.Feeds
 {
-    public class VisirFeed : NewsFeedBase
+    public class VisirFeed : NewsFeedBase<VisirItemBuilder>
     {
         public VisirFeed()
         {
             Url = "https://www.visir.is/rss/allt";
             FeedName = "Visir";
-        }
-
-        public override NewsItemBuilder BuildItem(SyndicationItem item)
-        {
-            return new VisirItemBuilder(item);
         }
     }
 }
