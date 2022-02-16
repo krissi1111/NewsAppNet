1ce8cdfdb673e48463efcfb8d115070602cc35c2
Ket1 <ket1@hi.is>
2022-02-16 10:55:15 +0000

    Added view model for Favorite class

---
+++ NewsAppNet/Models/ViewModels/FavoriteView.cs
@@ -0,0 +1,16 @@
+﻿using NewsAppNet.Models.DataModels;
+
+namespace NewsAppNet.Models.ViewModels
+{
+    public class FavoriteView
+    {
+        public int UserId { get; set; }
+        public int NewsItemId { get; set; }
+
+        public FavoriteView(Favorite fav)
+        {
+            UserId = fav.UserId;
+            NewsItemId = fav.NewsItemId;
+        }
+    }
+}
