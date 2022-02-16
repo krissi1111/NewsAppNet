49cb1fd6eab84e15ad7e0b50606a73770d68ac5c
Ket1 <ket1@hi.is>
2022-02-16 10:54:27 +0000

    Added data annotations

--- NewsAppNet/Models/DataModels/NewsItem.cs
+++ NewsAppNet/Models/DataModels/NewsItem.cs
@@ -1,16 +1,31 @@
 ﻿
 using NewsAppNet.Models.DataModels.Interfaces;
+using System.ComponentModel.DataAnnotations;
 
 namespace NewsAppNet.Models.DataModels
 {
     public class NewsItem : IEntityBase
     {
         public int Id { get; set; }
+
+        [Required]
         public string Origin { get; set; } = string.Empty;
+
+        [Required]
         public string Title { get; set; } = string.Empty;
+
+        [Required]
         public string Summary { get; set; } = string.Empty;
+
+        [Required]
+        [Url]
         public string Link { get; set; } = string.Empty;
+
+        [Required]
+        [Url]
         public string Image { get; set; } = string.Empty;
+
+        [Required]
         public DateTime Date { get; set; }
         public IList<Comment>? Comments { get; set; }
         public IList<Reply>? Replies { get; set; }
--- NewsAppNet/Models/DataModels/User.cs
+++ NewsAppNet/Models/DataModels/User.cs
@@ -1,14 +1,34 @@
 ﻿
 using NewsAppNet.Models.DataModels.Interfaces;
+using System.ComponentModel.DataAnnotations;
 
 namespace NewsAppNet.Models.DataModels
 {
     public class User : IEntityBase
     {
         public int Id { get; set; }
+
+        // Username must start with a letter
+        // Length 3 to 20
+        // Only letters from english alphabet or numbers
+        [Required]
+        [RegularExpression(@"^[a-zA-Z][a-zA-Z0-9]{2,19}$")]
         public string Username { get; set; } = string.Empty;
+
+        // First name has length 2 to 20
+        // Should allow letters from all western alphabets
+        [RegularExpression(@"^[a-zA-Zá-þÁ-Þ]{2,20}$")]
         public string FirstName { get; set; } = string.Empty;
+
+        // Last name has length 2 to 20
+        // Should allow letters from all western alphabets
+        [RegularExpression(@"^[a-zA-Zá-þÁ-Þ]{2,20}$")]
         public string LastName { get; set; } = string.Empty;
+
+        // Password has length 4+
+        // Only letters from english alphabet or numbers
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9]{4,}$")]
         public string Password { get; set; } = string.Empty;
         public string UserType { get; set; } = string.Empty;
         public IList<Comment>? Comments { get; set; }
