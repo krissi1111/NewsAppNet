fe001b046e6d8616fff41670f544fcc204af5531
Ket1 <ket1@hi.is>
2022-02-23 17:06:07 +0000

    Moved ServiceResponse to appropriate folder

--- NewsAppNet/Models/DataModels/ServiceResponse.cs
+++ NewsAppNet/Models/DataModels/ServiceResponse.cs
@@ -1,7 +1,9 @@
-﻿namespace NewsAppNet.Services
+﻿using NewsAppNet.Models.DataModels.Interfaces;
+
+namespace NewsAppNet.Models.DataModels
 {
     // Used for returning result from service layer to controller
-    public class ServiceResponse<T>
+    public class ServiceResponse<T> : IResponse
     {
         public T? Data { get; set; }
         public bool Success { get; set; } = true;
