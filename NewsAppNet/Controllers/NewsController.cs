ca527cca78193830b647039de236697090f2cd3e
Ket1 <ket1@hi.is>
2022-06-29 21:21:47 +0000

    Changed controller response handling

--- NewsAppNet/Controllers/NewsController.cs
+++ NewsAppNet/Controllers/NewsController.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using NewsAppNet.Models.DataModels;
 using NewsAppNet.Models.DataModels.Interfaces;
-using NewsAppNet.Models.ViewModels;
+using NewsAppNet.Models.DTOs;
 using NewsAppNet.Services.Interfaces;
 
 namespace NewsAppNet.Controllers
@@ -43,7 +43,7 @@ namespace NewsAppNet.Controllers
         {
             if (!serviceResponse.Success)
             {
-                return BadRequest(serviceResponse);
+                return BadRequest(serviceResponse.Message);
             }
             else
             {
@@ -54,26 +54,45 @@ namespace NewsAppNet.Controllers
         [HttpGet]
         public async Task<IActionResult> GetNews()
         {
-            ServiceResponse<List<NewsItemView>> serviceResponse = await newsService.GetNewsAll();
+            var response = await newsService.GetNewsAll();
+            
+            if (!response.Success)
+            {
+                return new BadRequestObjectResult(response.Message);
+            }
 
-            return HandleResponse(serviceResponse);
+            return Ok(response.Data);
         }
 
         // Returns news items based on search criteria
         [HttpPost("search")]
-        public async Task<IActionResult> GetNewsSearch([FromForm] Search search)
+        public async Task<IActionResult> GetNewsSearch([FromBody] Search search)
         {
-            ServiceResponse<List<NewsItemView>> serviceResponse = await newsService.GetNewsSearch(search);
+            var response = await newsService.GetNewsSearch(search);
 
-            return HandleResponse(serviceResponse);
+            if (!response.Success)
+            {
+                return new BadRequestObjectResult(response.Message);
+            }
+
+            return Ok(response.Data);
         }
 
         [HttpPost("feeds")]
         public async Task<IActionResult> GetNewsFeeds([FromForm] int[]? newsFeedIds)
         {
-            ServiceResponse<List<NewsFeedView>> serviceResponse = await newsFeedService.GetFeeds(newsFeedIds);
+            /*ServiceResponse<List<NewsFeedView>> serviceResponse = await newsFeedService.GetFeeds(newsFeedIds);
 
-            return HandleResponse(serviceResponse);
+            return HandleResponse(serviceResponse);*/
+
+            var response = await newsFeedService.GetFeeds(newsFeedIds);
+
+            if (!response.Success)
+            {
+                return new BadRequestObjectResult(response.Message);
+            }
+
+            return Ok(response.Data);
         }
 
         [Authorize]
@@ -81,7 +100,7 @@ namespace NewsAppNet.Controllers
         public async Task<IActionResult> AddNews()
         {
             int userId = GetUserId();
-            ServiceResponse<List<NewsItemView>> serviceResponse = await newsService.AddNews(userId);
+            ServiceResponse<List<NewsItemDTO>> serviceResponse = await newsService.AddNews(userId);
 
             return HandleResponse(serviceResponse);
         }
@@ -91,7 +110,7 @@ namespace NewsAppNet.Controllers
         public async Task<IActionResult> Delete([FromForm] int newsId)
         {
             int userId = GetUserId();
-            ServiceResponse<NewsItemView> serviceResponse = await newsService.DeleteNews(newsId, userId);
+            ServiceResponse<NewsItemDTO> serviceResponse = await newsService.DeleteNews(newsId, userId);
 
             return HandleResponse(serviceResponse);
         }
@@ -99,38 +118,37 @@ namespace NewsAppNet.Controllers
         [HttpPost("commentList")]
         public async Task<IActionResult> GetNewsCommentList([FromForm] int newsId)
         {
-            int userId = GetUserId();
-            ServiceResponse<List<CommentView>> serviceResponse = await commentReplyService.GetCommentList(newsId, userId);
+            var response = await commentReplyService.GetComments(newsId);
 
-            return HandleResponse(serviceResponse);
+            if (!response.Success)
+            {
+                return new BadRequestObjectResult(response.Message);
+            }
+
+            return Ok(response.Data);
         }
 
         [Authorize]
         [HttpPost("favAddRemove")]
-        public ActionResult<string> FavAddRemove([FromForm] int newsId)
+        public async Task<IActionResult> FavAddRemove([FromForm] int newsId)
         {
             int userId = GetUserId();
-            ServiceResponse<string> serviceResponse = favoriteService.AddRemoveFavorite(newsId, userId);
+            ServiceResponse<string> serviceResponse = await favoriteService.AddRemoveFavorite(newsId, userId);
 
-            return HandleResponse(serviceResponse);
-        }
-
-        [Authorize]
-        [HttpGet("userFav")]
-        public async Task<IActionResult> FavUserFavorites()
-        {
-            int userId = GetUserId();
-            ServiceResponse<List<FavoriteView>> serviceResponse = await favoriteService.GetUserFavorites(userId);
-            
             return HandleResponse(serviceResponse);
         }
 
         [HttpGet("popularComments")]
         public async Task<IActionResult> GetPopularNews()
         {
-            ServiceResponse<Dictionary<string, List<NewsItemView>>> serviceResponse = await newsService.GetPopularNews();
+            var response = await newsService.GetPopularNews();
 
-            return HandleResponse(serviceResponse);
+            if (!response.Success)
+            {
+                return new BadRequestObjectResult(response.Message);
+            }
+
+            return Ok(response.Data);
         }
 
         [Authorize]
@@ -138,7 +156,7 @@ namespace NewsAppNet.Controllers
         public async Task<IActionResult> Restore(int newsId)
         {
             int userId = GetUserId();
-            ServiceResponse<NewsItemView> serviceResponse = await newsService.RestoreNews(newsId, userId);
+            ServiceResponse<NewsItemDTO> serviceResponse = await newsService.RestoreNews(newsId, userId);
 
             return HandleResponse(serviceResponse);
         }
